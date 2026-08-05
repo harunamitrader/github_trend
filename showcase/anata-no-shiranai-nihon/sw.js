cf0c7bec55d02f090d570a89e8be9b919c2d4325
harunamitrader <sgmxk045@yahoo.co.jp>
2026-08-05 13:28:27 +0900

    Use red target reticle

--- showcase/anata-no-shiranai-nihon/sw.js
+++ showcase/anata-no-shiranai-nihon/sw.js
@@ -1,6 +1,6 @@
-const CACHE = 'anata-no-shiranai-nihon-v11';
+const CACHE = 'anata-no-shiranai-nihon-v12';
 const ASSETS = [
-  './', './index.html', './app.js?v=11', './style.css?v=11', './impact-fix.css?v=6', './manifest.webmanifest',
+  './', './index.html', './app.js?v=12', './style.css?v=12', './impact-fix.css?v=7', './manifest.webmanifest',
   './data/japan-prefectures.v1.json?v=2', './vendor/d3.min.js',
   './assets/story/01-alert.png', './assets/story/02-trajectory.png', './assets/story/03-intercept.png'
 ];
