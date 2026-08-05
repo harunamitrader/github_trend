a6d6a467e413c9efb77f3a2f97aaed65b9ad96fd
harunamitrader <sgmxk045@yahoo.co.jp>
2026-08-05 13:21:07 +0900

    Restore target size with thinner reticle

--- showcase/anata-no-shiranai-nihon/sw.js
+++ showcase/anata-no-shiranai-nihon/sw.js
@@ -1,6 +1,6 @@
-const CACHE = 'anata-no-shiranai-nihon-v10';
+const CACHE = 'anata-no-shiranai-nihon-v11';
 const ASSETS = [
-  './', './index.html', './app.js?v=10', './style.css?v=10', './impact-fix.css?v=5', './manifest.webmanifest',
+  './', './index.html', './app.js?v=11', './style.css?v=11', './impact-fix.css?v=6', './manifest.webmanifest',
   './data/japan-prefectures.v1.json?v=2', './vendor/d3.min.js',
   './assets/story/01-alert.png', './assets/story/02-trajectory.png', './assets/story/03-intercept.png'
 ];
